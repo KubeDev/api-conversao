13c4cea74f0ae6038e0b93d6805398df08c8b232
fabricioveronez <fabricioveronez@hotmail.com>
2021-01-25 15:58:51 -0300

    Urls antigas

--- index.js
+++ index.js
@@ -21,6 +21,22 @@ app.get('/celsius/:valor/fahrenheit', (req, res) => {
     res.json({ "fahrenheit": fahrenheit, "maquina": os.hostname() });
 });
 
+app.get('/temperatura/fahrenheitparacelsius/:valor', (req, res) => {
+
+    let valor = req.params.valor;
+    let celsius = (valor - 32) * 5 / 9;
+    res.json({ "celsius": celsius });
+});
+
+
+app.get('/temperatura/celsiusparafahrenheit/:valor', (req, res) => {
+
+    let valor = req.params.valor;
+    let fahrenheit = (valor * 9 / 5) + 32;
+    res.json({ "fahrenheit": fahrenheit });
+});
+
+
 app.listen(8080, () => {
     console.log("Servidor rodando na porta 8080");
 });
