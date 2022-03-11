b0b3b55b35a2dfce00833a3b9e63e9de39c5e508
Bruno Botelho <bruno.botelho.br@gmail.com>
2022-03-11 13:16:23 -0300

    .

---
+++ .test/qa.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+#sleep 10
+echo "API Simple HTTP Test"
+
+# Test: Redness
+URL=https://api-service-brunobotelhobr.cloud.okteto.net/ready
+STATUSCODE=$(curl -s -o /dev/null -w "%{http_code}" -X GET $URL -H  "accept: application/json")
+if test $STATUSCODE != 200; then
+    exit 1
+    echo $URL " | HTTP Code: " $STATUSCODE " | Test Status Fail"
+fi
+echo $URL " | HTTP Code: " $STATUSCODE " | Test Status Pass"
+
+# Test: Temperature Conversion 
+URL=https://api-service-brunobotelhobr.cloud.okteto.net/celsius/10/fahrenheit
+STATUSCODE=$(curl -s -o /dev/null -w "%{http_code}" -X GET $URL -H  "accept: application/json")
+if test $STATUSCODE != 200; then
+    exit 1
+    echo $URL " | HTTP Code: " $STATUSCODE " | Test Status ail"
+fi
+echo $URL " | HTTP Code: " $STATUSCODE " | Test Status Pass"
+
+URL=https://api-service-brunobotelhobr.cloud.okteto.net/fahrenheit/10/celsius
+STATUSCODE=$(curl -s -o /dev/null -w "%{http_code}" -X GET $URL -H  "accept: application/json")
+if test $STATUSCODE != 200; then
+    exit 1
+    echo $URL " | HTTP Code: " $STATUSCODE " | Test Status Fail"
+fi
+echo $URL " | HTTP Code: " $STATUSCODE " | Test Status Pass"
